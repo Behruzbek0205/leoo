cfca8bc258841fef3ef56cc4d9896356d04aef8a
Bexruzbek Hasanbayev <bexruzbekhasanbayev@gmail.com>
2025-12-23 20:33:30 +0500

    leeo js

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ const num = [12, 5, 7, 9, 3];
 console.log(num.splice(4));
 // slice vazifasi kesib berish , manfiyniham kesib beradi yani -1
 
+const un = [12, 5, 7, 9, 3];
+console.log(un.indexOf(12));
+// indexOf nechinchi indexdagiligini topadi
+
