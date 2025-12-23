df47a9665f72c2592608a648e67c137dc6aa403c
Bexruzbek Hasanbayev <bexruzbekhasanbayev@gmail.com>
2025-12-23 20:37:00 +0500

    2121

--- app.js
+++ app.js
@@ -1,45 +1,107 @@
-const num = [12, 5, 7, 9, 3];
-console.log(num.splice(4));
-// slice vazifasi kesib berish , manfiyniham kesib beradi yani -1
+// const num = [12, 5, 7, 9, 3];
+// console.log(num.splice(4));
+// // slice vazifasi kesib berish , manfiyniham kesib beradi yani -1
 
-const un = [12, 5, 7, 9, 3];
-console.log(un.indexOf(12));
-// indexOf nechinchi indexdagiligini topadi
+// const un = [12, 5, 7, 9, 3];
+// console.log(un.indexOf(12));
+// // indexOf nechinchi indexdagiligini topadi
 
-const katta = "frontend";
-console.log(katta.toUpperCase());
-// toUpperCase hamma sozni kattalashtiradi
+// const katta = "frontend";
+// console.log(katta.toUpperCase());
+// // toUpperCase hamma sozni kattalashtiradi
 
-for (i = 0; i <= 10; i++) {
-  console.log(i);
-}
-//  for chiklidan foydalanib ishladik
-
-const meva = ["olma", "gilos"];
-meva.push("nok");
-console.log(meva);
-// console.log(meva + ',nok');
-// push oxirigs qoshib berdi
-
-//  alert, promt, confirm
-
-// alert userni ogohlantirish sifatida ishlatiladi
-// alert('nma gap')
-
-// promt bu userdan malumot olsih uchun ishlatiladi
-// let promt = prompt("yoshingizni kiriting");
-// if (promt >= 18) {
-//   console.log("ktish mumkin");
-// } else {
-//   console.log("hali yosh siz");
+// for (i = 0; i <= 10; i++) {
+//   console.log(i);
 // }
+// //  for chiklidan foydalanib ishladik
+
+// const meva = ["olma", "gilos"];
+// meva.push("nok");
+// console.log(meva);
+// // console.log(meva + ',nok');
+// // push oxirigs qoshib berdi
+
+// //  alert, promt, confirm
+
+// // alert userni ogohlantirish sifatida ishlatiladi
+// // alert('nma gap')
+
+// // promt bu userdan malumot olsih uchun ishlatiladi
+// // let promt = prompt("yoshingizni kiriting");
+// // if (promt >= 18) {
+// //   console.log("ktish mumkin");
+// // } else {
+// //   console.log("hali yosh siz");
+// // }
 
 
 
-const kopay = [1, 2, 3, 4];
-console.log(kopay.map((item) => item * 10));
-// map blan ishladik va map vazifasi har bir valueni ichiga kirib 10ga kopaytirib chiqiyapti
+// const kopay = [1, 2, 3, 4];
+// console.log(kopay.map((item) => item * 10));
+// // map blan ishladik va map vazifasi har bir valueni ichiga kirib 10ga kopaytirib chiqiyapti
 
-let uzunlik = "JavsScript";
-console.log(uzunlik.length);
+// let uzunlik = "JavsScript";
+// console.log(uzunlik.length);
 
+// const boolean = "banan";
+// console.log(!boolean);
+// // ! belgisi boolean qiymatni teskari aylantiradi
+
+// const teskari = "hello";
+// console.log(teskari.split("").reverse().join(""));
+// //  split('') bu harbir harifni ajratib oladi .reverse() teskari qiladi join('') stringa ogirib beradi
+
+// // 11 misol qolib ketti
+
+let son = [3, 6, 34, 66, 234];
+console.log(Math.max(...son));
+
+//  13 misol
+// const kata = [3, 6, 34, 66];
+// let numm = kata[0];
+// for (let i = 1; i < kata.length; i++) {
+//     if(kata[i] > numm){
+//         numm = kata[i]
+//     }
+// }
+// console.log(numm);
+
+
+const hammasi  = 'Men dasturchilarni yaxshi koraman'
+const sozlar = hammasi.split(' ')
+console.log(sozlar.length);
+
+
+let arr = ['js', 'frontend', 'web']
+console.log(arr.sort());
+const boolean = "banan";
+console.log(!boolean);
+// ! belgisi boolean qiymatni teskari aylantiradi
+
+const teskari = "hello";
+console.log(teskari.split("").reverse().join(""));
+//  split('') bu harbir harifni ajratib oladi .reverse() teskari qiladi join('') stringa ogirib beradi
+
+// 11 misol qolib ketti
+
+let son = [3, 6, 34, 66, 234];
+console.log(Math.max(...son));
+
+//  13 misol
+// const kata = [3, 6, 34, 66];
+// let numm = kata[0];
+// for (let i = 1; i < kata.length; i++) {
+//     if(kata[i] > numm){
+//         numm = kata[i]
+//     }
+// }
+// console.log(numm);
+
+
+const hammasi  = 'Men dasturchilarni yaxshi koraman'
+const sozlar = hammasi.split(' ')
+console.log(sozlar.length);
+
+
+let arr = ['js', 'frontend', 'web']
+console.log(arr.sort());
