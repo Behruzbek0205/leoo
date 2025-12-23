b3bc16fb98cf7aef62c7ffdd1b2f743b712eccea
Bexruzbek Hasanbayev <bexruzbekhasanbayev@gmail.com>
2025-12-23 20:33:50 +0500

    leeo js

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ const un = [12, 5, 7, 9, 3];
 console.log(un.indexOf(12));
 // indexOf nechinchi indexdagiligini topadi
 
+const katta = "frontend";
+console.log(katta.toUpperCase());
+// toUpperCase hamma sozni kattalashtiradi
+
